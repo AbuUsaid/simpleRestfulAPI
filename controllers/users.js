4d0c76ec3f437832f990df000d5de904b812553b
AbuUsaid <84924763+AbuUsaid@users.noreply.github.com>
2022-09-17 17:59:13 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 let users = [];
-
+//Note: We export all the functions from here i.e the logical part so that it can be used by the routes.
 export const getUsers = (req, res) => {
   res.send(users);
 };
